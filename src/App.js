29fd94c52d2154576b6910d569840cae3de702e1
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2023-11-28 17:35:51 +0330

    4. Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+import ChatFeed from "./components/ChatFeed";
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
         projectID="5872346f-4809-4ff9-9e66-4957ac68e886"
         userName="Mehdi"
         userSecret="qwer"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() =>
+          new Audio(
+            "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+          ).play()
+        }
       />
     </div>
   );
