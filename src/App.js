d8246efb37e9acedab945bb3958f856c1d788df9
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2023-11-28 20:32:09 +0330

    Chat Feed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+// This component utilizes the 'ChatEngine' component from 'react-chat-engine' to set up a chat application. Here are comments for each section:
+
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 import ChatFeed from "./components/ChatFeed";
 
 function App() {
   return (
+    // Main container for the chat application
     <div className="App">
+      {/* 'ChatEngine' component for handling chat functionalities with various props */}
       <ChatEngine
-        height="100vh"
-        projectID="5872346f-4809-4ff9-9e66-4957ac68e886"
-        userName="Mehdi"
-        userSecret="qwer"
+        height="100vh" // Set the height of the ChatEngine component
+        projectID="5872346f-4809-4ff9-9e66-4957ac68e886" // Unique project ID for authentication and accessing chat data
+        userName="Mehdi" // User's username for authentication
+        userSecret="qwer" // User's secret or password for authentication
+        // 'renderChatFeed' function to customize the chat feed appearance by passing props to the 'ChatFeed' component.
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        // Action triggered when a new message arrives
         onNewMessage={() =>
           new Audio(
             "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,7 +1,64 @@
-const ChatFeed = (props) => {
-  console.log(props);
+// This component serves as a chat feed and message display section.
 
-  return <div>ChatFeed component</div>;
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+// 'ChatFeed' component responsible for displaying chat messages
+const ChatFeed = (props) => {
+  const { chats, activeChat, userName, messages } = props; // Destructuring props to extract required data
+
+  const chat = chats && chats[activeChat]; // Retrieve the active chat object
+
+  // Function to render chat messages
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+
+    // Mapping through each message and rendering MyMessage or TheirMessage components
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index];
+      const isMyMessage = userName === message.sender.userName;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isMyMessage ? (
+              <MyMessage message={message} /> // Displaying user's own message
+            ) : (
+              <TheirMessage // Displaying messages from other users
+                message={message}
+                lastMessage={messages[lastMessageKey]}
+              />
+            )}
+          </div>
+
+          {/* Component for read receipts */}
+          <div
+            className="read-reciepts"
+            style={{
+              marginRight: isMyMessage ? "18px" : "0px",
+              marginLeft: isMyMessage ? "0px" : "68px",
+            }}
+          ></div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle"></div>
+        {renderMessages()}
+        <div style={{ height: "100px" }} />
+        <div className="message-form-container">
+          <MessageForm {...props} chatId={activeChat} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatFeed;
