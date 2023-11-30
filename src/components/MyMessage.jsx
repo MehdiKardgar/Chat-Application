ba15f79c0275f608e91909ac20f131c723d57caf
Mehdi Kardgar <76148286+MehdiKardgar@users.noreply.github.com>
2023-11-30 12:15:59 +0330

    My Message Component

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,5 +1,27 @@
-const MyMessage = () => {
-  return <div>MyMessage component</div>;
+const MyMessage = ({ message }) => {
+  if (message.attachments && message.attachments > 0) {
+    return (
+      <img
+        className="message-image"
+        src={message.attachment[0].file}
+        alt="message attachment"
+        style={{ float: "right" }}
+      />
+    );
+  }
+
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        backgroundColor: "#3B2A50",
+      }}
+    >
+      {message.text}
+    </div>
+  );
 };
 
 export default MyMessage;
